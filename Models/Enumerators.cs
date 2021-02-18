9b61792e11ddc796e065299d163d4c35433fd5de
Diego Ramos Claumann <contatodclaumann@gmail.com>
2021-02-18 19:20:09 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Enumerators.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SystemRH.Models
+{
+    public class Enumerators
+    {
+        public enum typeLogin { Administrator,Recruiter,Candidate}
+    }
+}
---
+++ Models/Login.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SystemRH.Models
+{
+    public class Login
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required(ErrorMessage ="Usuário Obrigatório",AllowEmptyStrings = false)]
+        [Display(Name = "Usuário")]
+        public String username { get; set; }
+
+        [Required(ErrorMessage = "Senha Obrigatória", AllowEmptyStrings = false)]
+        [Display(Name = "Senha")]
+        public String password { get; set; }
+        [Required(ErrorMessage = "E-mail Obrigatório", AllowEmptyStrings = false)]
+        [Display(Name = "E-mail")]
+        public String email { get; set; }
+        [Required(ErrorMessage = "Tipo de Login Obrigatório", AllowEmptyStrings = false)]
+        [Display(Name = "Tipo de Login")]
+        public Enumerators.typeLogin typeLogin { get; set; }
+        [Display(Name = "Situação")]
+        public char active { get; set; }
+
+    }
+}
